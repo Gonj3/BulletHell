409bd713dc88d7bdeb7ec655380a9607771402a5
Hussein Aljanaby <husseinalaa.aljanaby@gmail.com>
2023-09-04 12:35:15 +1200

    Fixed Conventions
    Fixed field conventions to PascalCase.
    Added const keyword to constant fields.

--- Load.cs
+++ Load.cs
@@ -4,7 +4,11 @@ using System;
 public partial class Load : Control
 {
 	//Main Menu scene source.
+<<<<<<< Updated upstream
 	private String mainScene = "res://MainMenu.tscn";
+=======
+	private const String MainScene = "res://MainMenu.tscn";
+>>>>>>> Stashed changes
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -19,6 +23,10 @@ public partial class Load : Control
 	//Change back to Main Menu scene.
 	public void _on_back_button_pressed() 
 	{
+<<<<<<< Updated upstream
 		GetTree().ChangeSceneToFile(mainScene);
+=======
+		GetTree().ChangeSceneToFile(MainScene);
+>>>>>>> Stashed changes
 	}
 }
