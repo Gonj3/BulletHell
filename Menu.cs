b1841ad99154afba2c9958f0ca776f6bed0281ad
Hussein Aljanaby <husseinalaa.aljanaby@gmail.com>
2023-09-04 12:53:24 +1200

    Fixed Merge Issues

--- Menu.cs
+++ Menu.cs
@@ -4,19 +4,11 @@ using System;
 public partial class Menu : Control
 {
 	//TODO ADD GAME SCENE SOURCE.
-<<<<<<< Updated upstream
-	private String gameScene = "res://Game.tscn";
-	//TEMP Settings Scene source.
-	private String settings = "res://Settings.tscn";
-	//Load Scene source.
-	private String load = "res://Load.tscn";
-=======
 	private const String GameScene = "res://Game.tscn";
 	//TEMP Settings Scene source.
 	private const String Settings = "res://Settings.tscn";
 	//Load Scene source.
 	private const String Load = "res://Load.tscn";
->>>>>>> Stashed changes
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -32,31 +24,19 @@ public partial class Menu : Control
 	//Enter Game scene.
 	public void _on_start_button_pressed() 
 	{
-<<<<<<< Updated upstream
-		GetTree().ChangeSceneToFile(gameScene);
-=======
 		GetTree().ChangeSceneToFile(GameScene);
->>>>>>> Stashed changes
 	}
 
 	//Enter Load scene.
 	public void _on_load_button_pressed()
 	{
-<<<<<<< Updated upstream
-		GetTree().ChangeSceneToFile(load);
-=======
 		GetTree().ChangeSceneToFile(Load);
->>>>>>> Stashed changes
 	}
 
 	//Enter Settings scene.
 	public void _on_settings_button_pressed()
 	{
-<<<<<<< Updated upstream
-		GetTree().ChangeSceneToFile(settings);
-=======
 		GetTree().ChangeSceneToFile(Settings);
->>>>>>> Stashed changes
 	}
 
 	//Quit game.
--- Settings.cs
+++ Settings.cs
@@ -4,11 +4,7 @@ using System;
 public partial class Settings : Control
 {
 	//Main Menu scene source.
-<<<<<<< Updated upstream
-	private String mainScene = "res://MainMenu.tscn";
-=======
 	private const String MainScene = "res://MainMenu.tscn";
->>>>>>> Stashed changes
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -23,10 +19,6 @@ public partial class Settings : Control
 	//Change back to Main Menu scene.
 	public void _on_back_pressed() 
 	{
-<<<<<<< Updated upstream
-		GetTree().ChangeSceneToFile(mainScene);
-=======
 		GetTree().ChangeSceneToFile(MainScene);
->>>>>>> Stashed changes
 	}
 }
