d5ab7fd112c8e5dd974471dcfa06b9fcf97fcd0f
jamesp6784 <105037949+jamesp6784@users.noreply.github.com>
2023-09-06 21:42:34 +1200

    Integrate with main menu

--- SaveGameTesting.cs
+++ SaveGameTesting.cs
@@ -3,33 +3,40 @@ using System;
 
 public partial class SaveGameTesting : VBoxContainer
 {
-    private SaveGame SaveGame;
-    private LineEdit NameBox;
-    private SpinBox DeathsBox;
-    private SpinBox KillsBox;
+	private const String MainMenu = "res://MainMenu.tscn";
+	
+	private SaveGame SaveGame;
+	private LineEdit NameBox;
+	private SpinBox DeathsBox;
+	private SpinBox KillsBox;
 
-    public override void _Ready()
-    {
-        SaveGame = GetNode<SaveGame>("/root/SaveGame");
-        NameBox = GetNode<LineEdit>("NameBox");
-        DeathsBox = GetNode<SpinBox>("DeathsBox");
-        KillsBox = GetNode<SpinBox>("KillsBox");
+	public override void _Ready()
+	{
+		SaveGame = GetNode<SaveGame>("/root/SaveGame");
+		NameBox = GetNode<LineEdit>("NameBox");
+		DeathsBox = GetNode<SpinBox>("DeathsBox");
+		KillsBox = GetNode<SpinBox>("KillsBox");
 
-        var p = SaveGame.Profile;
+		var p = SaveGame.Profile;
 
-        NameBox.Text = p.Name;
-        DeathsBox.Value = p.Deaths;
-        KillsBox.Value = p.Kills;
-    }
+		NameBox.Text = p.Name;
+		DeathsBox.Value = p.Deaths;
+		KillsBox.Value = p.Kills;
+	}
 
-    private void _on_save_btn_pressed()
-    {
-        var p = SaveGame.Profile;
+	private void _on_save_btn_pressed()
+	{
+		var p = SaveGame.Profile;
 
-        p.Name = NameBox.Text;
-        p.Deaths = (uint)DeathsBox.Value;
-        p.Kills = (uint)KillsBox.Value;
+		p.Name = NameBox.Text;
+		p.Deaths = (uint)DeathsBox.Value;
+		p.Kills = (uint)KillsBox.Value;
 
-        SaveGame.Save();
-    }
+		SaveGame.Save();
+	}
+	
+	private void _on_back_btn_pressed()
+	{
+		GetTree().ChangeSceneToFile(MainMenu);
+	}
 }
