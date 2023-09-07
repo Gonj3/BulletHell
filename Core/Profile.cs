6853a964a10889b56aae03b08bba22f19f87bf01
jamesp6784 <105037949+jamesp6784@users.noreply.github.com>
2023-09-07 23:59:43 +1200

    Tabs instead lol

--- Core/Profile.cs
+++ Core/Profile.cs
@@ -2,21 +2,21 @@ using Godot;
 
 public class Profile
 {
-    public string Name { get; set; }
+	public string Name { get; set; }
 
-    public uint Deaths { get; set; }
+	public uint Deaths { get; set; }
 
-    public uint Kills { get; set; }
+	public uint Kills { get; set; }
 
-    public static Profile FromFile(FileAccess file)
-    {
-        return new Profile { Name = file.GetPascalString(), Deaths = file.Get32(), Kills = file.Get32() };
-    }
+	public static Profile FromFile(FileAccess file)
+	{
+		return new Profile { Name = file.GetPascalString(), Deaths = file.Get32(), Kills = file.Get32() };
+	}
 
-    public void WriteToFile(FileAccess file)
-    {
-        file.StorePascalString(Name);
-        file.Store32(Deaths);
-        file.Store32(Kills);
-    }
+	public void WriteToFile(FileAccess file)
+	{
+		file.StorePascalString(Name);
+		file.Store32(Deaths);
+		file.Store32(Kills);
+	}
 }
