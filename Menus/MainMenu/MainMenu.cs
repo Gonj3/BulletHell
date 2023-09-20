85a8f7ed83e101015421559f72561e07ddeb72f0
Hussein Aljanaby <husseinalaa.aljanaby@gmail.com>
2023-09-21 08:08:59 +1200

    Fixed Intro Re-Playing
    Intro no longer replays when loaded by exiting settings menu.
    Fixed via intro song loaded onto an AudioStreamPlayer inside root node.
    Intro pauses on player scene load.

--- Menus/MainMenu/MainMenu.cs
+++ Menus/MainMenu/MainMenu.cs
@@ -5,14 +5,20 @@ public partial class MainMenu : Control
 	[Export]
 	private Button DefaultButton;
 
+	private AudioStreamPlayer IntroPlayer;
 	public override void _Ready()
 	{
+		IntroPlayer =  this.GetRoot().GetNode<AudioStreamPlayer>("IntroPlayer");
+		if(!IntroPlayer.Playing) {
+			IntroPlayer.Play();
+		}
 		DefaultButton.GrabFocus();
 	}
 
 	// Enter Game scene.
 	public void _on_start_button_pressed()
 	{
+		IntroPlayer.Playing = false;
 		this.GetRoot().SetScene(Scene.TestWorld);
 	}
 
