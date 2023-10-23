b0155e5f7209f6d5c3cc4078b76da9b5c87c458a
jamesp6784 <105037949+jamesp6784@users.noreply.github.com>
2023-10-23 15:57:36 +1300

    Basic player shooting

---
+++ Game/World.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+public partial class World : Node2D
+{
+	private PackedScene projectileScene;
+
+	public override void _Ready()
+	{
+		projectileScene = GD.Load<PackedScene>("res://Game/Entities/Projectile.tscn");
+	}
+
+	public void SpawnProjectile(Vector2 pos, float angle, float speed, DamageableKind target)
+	{
+		var projInstance = (Projectile)projectileScene.Instantiate();
+
+		projInstance.Position = pos;
+		projInstance.Angle = angle;
+		projInstance.Speed = speed;
+		projInstance.Target = target;
+
+		GetParent().AddChild(projInstance);
+	}
+}
