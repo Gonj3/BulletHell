ec24338422e5e6eea3bb3603183f36e105c45993
qrq1356 <qrq1356@autuni.ac.nz>
2023-10-23 17:17:39 +1300

    Boss Firing

--- Game/Entities/Boss.cs
+++ Game/Entities/Boss.cs
@@ -5,14 +5,15 @@ public partial class Boss : RigidBody2D, IDamageable
 {
 	public float Speed = 10f;
 	public int Health = 200;
-	private float ProjectileSpeed = 2f;
-	private int ProjectileCount = 10;
+	private float ProjectileSpeed = 50f;
+	private int ProjectileCount = 8;
+	private int fireCount = 0;
 	private PackedScene projectileScene;
 	private Timer FireTimer;
 	private Timer AltFireTimer;
 	[Export]
 	private Player player;
-	public DamageableKind DamageableKind { get; } = DamageableKind.Boss;
+	public DamageableKind DamageableKind { get; } = DamageableKind.Enemy;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -24,7 +25,7 @@ public partial class Boss : RigidBody2D, IDamageable
 	private void InitTimers()
 	{
 		FireTimer = GetNode<Timer>("FireTimer");
-		FireTimer.WaitTime = 1f;
+		FireTimer.WaitTime = 0.7f;
 		FireTimer.Start();
 
 		AltFireTimer = GetNode<Timer>("AltFireTimer");
@@ -33,31 +34,35 @@ public partial class Boss : RigidBody2D, IDamageable
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		LinearVelocity = Position.DirectionTo(player.Position) * Speed;
 	}
 	
 	public void _OnFireTimerTimeout()
 	{
-		FireSpin();
+		Fire();
 	}
 
 	public void _OnAltFireTimerTimeout()
 	{
-		FireSpin();	
+		// Todo: adaptable projectile types.
 	}
 
-	// a fire method similar to the enemys the spread, that slowly spins around the boss
-	private void FireSpin()
-	{
-		for (int i = 0; i < ProjectileCount; i++)
-		{
-			float angleOffset = (float)(2 * Math.PI / ProjectileCount * i);
-			FireProjectiles(angleOffset);
-		}
-	}
-
+private void Fire()
+{
+    var angleStep = Mathf.Pi * 2 / ProjectileCount;
+    var angleOffset = fireCount * angleStep / 2;
+    for (var i = 0; i < ProjectileCount; i++)
+    {
+        Projectile projectile = (Projectile)projectileScene.Instantiate();
+        projectile.Position = Position;
+        projectile.Angle = angleStep * i + angleOffset + Rotation;
+        projectile.Speed = ProjectileSpeed;
+        GetTree().Root.AddChild(projectile);
+    }
+    fireCount++;
+}
 	private void FireProjectiles(float angleOffset)
 	{
 		var projInstance = (Projectile)projectileScene.Instantiate();
